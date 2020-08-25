f6b9ed06a48246c2658452753b4d8cb6f496202e
Giuseppe <giuseppedf8711@gmail.com>
2020-08-25 15:51:30 +0200

    inizio

---
+++ script.js
@@ -0,0 +1,9 @@
+// Lista Cognomi
+// Chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+var lista = ["Bianchi", "Rossi", "Duzioni", "Balsamo", "Verdi"];
+
+var cognome = prompt("Inserisci il tuo cognome");
