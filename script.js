d5d46911c70d00c76efc255e077329c7be462298
Giuseppe <giuseppedf8711@gmail.com>
2020-08-25 22:29:14 +0200

    fine

--- script.js
+++ script.js
@@ -4,6 +4,21 @@
 // stampa la lista ordinata alfabeticamente
 // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 
-var lista = ["Bianchi", "Rossi", "Duzioni", "Balsamo", "Verdi"];
+// array con all'interno dei cognomi
+var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsamo", "Verdi"];
 
+// chiedo all'utente di inserire il suo cognome
 var cognome = prompt("Inserisci il tuo cognome");
+
+// inserisco il cognome dell'utente all'interno dell' array
+cognomi.push(cognome);
+
+// ordino la lista dei cognomi in ordine alfabetico e la stampo
+cognomi.sort();
+console.log(cognomi);
+
+// indico la posizione "umana" del cognome inserito e la stampo
+
+// cambio la posizione da "informatica" ad "umana" aggiungendo +1
+var posizione = (cognomi.indexOf(cognome) + 1);
+console.log("Il tuo cognome è in posizione: " + posizione);
